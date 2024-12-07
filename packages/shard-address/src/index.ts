cb89e79f14596739571f9d2d900460ce0e89724a
newtmex <brightomex@gmail.com>
2024-12-07 16:09:04 +0100

    with mnemonic and private key

--- packages/shard-address/src/index.ts
+++ packages/shard-address/src/index.ts
@@ -1,26 +1,36 @@
 #!/usr/bin/env node
 
-import { Address, AddressComputer, Mnemonic } from "@multiversx/sdk-core/out";
+import {
+    Address,
+    AddressComputer,
+    Mnemonic,
+    UserSecretKey,
+} from "@multiversx/sdk-core/out";
 
 const addressComputer = new AddressComputer();
 
 // addressess[shard] = Address[]
-const addresses: Address[][] = [[], [], []];
+const addresses: { address: string; mnemonic: string; privateKey: string }[][] =
+    [[], [], []];
 
 while (addresses.flatMap((address) => address).length < 9) {
-    const address = new Address(Mnemonic.generate().getEntropy());
+    const mnemonic = Mnemonic.generate();
+    const privateKey = mnemonic.deriveKey().hex();
+    const address = new Address(mnemonic.getEntropy());
     const shard = addressComputer.getShardOfAddress(address);
 
     if (addresses[shard].length < 3) {
-        addresses[shard].push(address);
+        addresses[shard].push({
+            address: address.toBech32(),
+            privateKey,
+            mnemonic: mnemonic.toString(),
+        });
     }
 }
 
 const addressObj = addresses.reduce((addressObj, shardAddress, shard) => {
-    addressObj[shard as 0 | 1 | 2] = shardAddress.map((address) =>
-        address.toBech32()
-    );
+    addressObj[shard as 0 | 1 | 2] = shardAddress.map((address) => address);
     return addressObj;
-}, {} as { [key in 0 | 1 | 2]: string[] });
+}, {} as { [key in 0 | 1 | 2]: (typeof addresses)[number] });
 
 console.log(addressObj);
