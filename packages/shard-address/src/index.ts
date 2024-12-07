662d937450ed445c7251fe2134fea68959040e26
newtmex <brightomex@gmail.com>
2024-12-07 15:31:18 +0100

    Creating address by shard

--- packages/shard-address/src/index.ts
+++ packages/shard-address/src/index.ts
@@ -1,2 +1,26 @@
 #!/usr/bin/env node
-console.log("Hello, MultiversX!");
+
+import { Address, AddressComputer, Mnemonic } from "@multiversx/sdk-core/out";
+
+const addressComputer = new AddressComputer();
+
+// addressess[shard] = Address[]
+const addresses: Address[][] = [[], [], []];
+
+while (addresses.flatMap((address) => address).length < 9) {
+    const address = new Address(Mnemonic.generate().getEntropy());
+    const shard = addressComputer.getShardOfAddress(address);
+
+    if (addresses[shard].length < 3) {
+        addresses[shard].push(address);
+    }
+}
+
+const addressObj = addresses.reduce((addressObj, shardAddress, shard) => {
+    addressObj[shard as 0 | 1 | 2] = shardAddress.map((address) =>
+        address.toBech32()
+    );
+    return addressObj;
+}, {} as { [key in 0 | 1 | 2]: string[] });
+
+console.log(addressObj);
